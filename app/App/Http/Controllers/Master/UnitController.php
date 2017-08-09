8deeb04b51e2a14055c6d41d987aba5ea4fc4d37
masmike <ddtika@gmail.com>
2017-08-09 07:23:47 +0700

    unit

--- app/App/Http/Controllers/Master/UnitController.php
+++ app/App/Http/Controllers/Master/UnitController.php
@@ -11,8 +11,8 @@ class UnitController extends Controller
     public function get()
     {
     	$unit = Unit::all();
-    	$merk = Merk::all();
-    	$type = Type::all();
+    	$merk = Merk::where('status', '1')->get();
+    	$type = Type::where('status', '1')->get();
 
     	$data = array('page' => 'dashboard.master.unit', 'allUnit' => $unit, 'allMerk' => $merk, 'allType' => $type);
         return $this->render('dashboard/master/unit/list', $data);
@@ -50,9 +50,37 @@ class UnitController extends Controller
     public function getDetail($unitId)
     {
         $unit = Unit::with(['merk', 'type'])->where('id', $unitId)->first();
+        $columnId = Unit::select('merk_id', 'type_id')->where('id', $unitId)->first();
+        $merk = Merk::where([['status', '=', '1'], ['id', '!=', $columnId['merk_id']]])->get();
+        $type = Type::where([['status', '=', '1'], ['id', '!=', $columnId['type_id']]])->get();
 
-        $data = array('page' => 'dashboard.master.unit.edit', 'unit' => $unit);
-       // var_dump(json_encode($data));
+        $data = array('page' => 'dashboard.master.unit.edit', 'unit' => $unit, 'allMerk' => $merk, 'allType' => $type);
+        //var_dump(json_encode($data));
         return $this->render('dashboard/master/unit/edit', $data);
+    }
+
+    public function postDetail()
+    {
+        $uId = $this->param('uid');
+        $uMerk = $this->param('umerk');
+        $uKode = $this->param('ukode');
+        $uType = $this->param('utype');
+        $uHarga = $this->param('uharga');
+        $uDetail = $this->param('udetail');
+        $uStatus = $this->param('statusActive');
+
+        $unit = Unit::where('id', $uId)->first();
+
+        $unit->update([
+            'merk_id' => $uMerk,
+            'kode' => $uKode,
+            'type_id' => $uType,
+            'harga' => $uHarga,
+            'deskripsi' => $uDetail,
+            'status' => $uStatus
+            ]);
+
+        return json_encode(array('status' => 'OK'));
+
     }
 }
